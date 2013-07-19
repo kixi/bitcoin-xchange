af638ad076fd174f392c550c83136cff30238a67
Kixi <kixi73@gmail.com>
2013-07-20 00:09:40 +0200

    m

---
+++ src/main/scala/util/RedisEventStore.scala
@@ -0,0 +1,12 @@
+package util
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: guenter
+ * Date: 07.07.13
+ * Time: 23:24
+ * To change this template use File | Settings | File Templates.
+ */
+class RedisEventStore {
+
+}
---
+++ src/test/scala/domain/account/SpecTest.scala
@@ -0,0 +1,12 @@
+package domain.account
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: guenter
+ * Date: 19.07.13
+ * Time: 23:28
+ * To change this template use File | Settings | File Templates.
+ */
+class SpecTest {
+
+}
