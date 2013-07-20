23796a9e70df051149a2b8cec9bd879a539f2891
KixIT <kixi73@gmail.com>
2013-07-20 10:41:17 +0100

    changes

---
+++ src/main/scala/service/Bootstrap.scala
@@ -0,0 +1,12 @@
+package service
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: guenter
+ * Date: 20.07.13
+ * Time: 10:12
+ * To change this template use File | Settings | File Templates.
+ */
+class Bootstrap {
+
+}
